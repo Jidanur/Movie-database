26ee9efc999fe2c772925c500d89b253c895774b
JIDAN99 <jidanurrahman99@gmail.com>
2022-06-19 23:45:23 -0500

    added result window to show search results

--- movieInterface.py
+++ movieInterface.py
@@ -1,9 +1,27 @@
 import sqlite3
+from tkinter import *
 import tkinter as tk
-
-from tkinter import ttk
+from tkinter import Tk, ttk
 from tkinter.messagebox import showinfo
+from tabulate import tabulate
 
+tabulate.PRESERVE_WHITESPACE = True
+
+
+#function for exporting results in CSV file
+def exportCSV(lst):
+    contents = ""
+    out = open("export.csv","w",encoding="utf-8")
+
+    for r in range(len(lst)):
+        for c in range(len(lst[r])):
+            contents += str(lst[r][c]) + ","
+        contents +="\n"
+        
+    out.write(contents)
+    out.close()
+
+    showinfo("DONE","Succesfully Exported to export.csv")
 
 
 
@@ -17,7 +35,48 @@ def resetCombos():
 def searchMovie():
     msg = f"name: {MovieName.get()} \nyear: {select_year.get()} \nproductionCOMP:{producedBy.get()} \ncountry:{productionCountry.get()}"
 
-    showinfo(title= "info" , message= msg)
+
+    resultWindow = Tk()
+    resultWindow.title("Search results")
+    resultWindow.geometry("1280x720")
+    
+    #text box to show search results 
+    text_box = Text(resultWindow,font="ariel",height=100,width= 250)
+    scroll_bar = Scrollbar(resultWindow,orient="vertical",command=text_box.yview)
+    scroll_bar.pack(side=RIGHT,expand=True,fill="y")
+    text_box.configure(yscrollcommand=scroll_bar.set)
+    text_box.pack(fill=X,expand= True)
+    
+
+    #inserting results to textBox
+    sql_cmd = f"""SELECT title, release_date, budget, revenue, prodCompany.name as productionCompany, prodCountry.name as countryOfProduction, Genre.name as Genre from movie 
+        join prodCompany on prodCompany.id = movie.id
+        join prodCountry on prodCountry.id = movie.id
+        join Genre on Genre.id = movie.id
+        where title like '{MovieName.get()}%' and
+        release_date like '%{select_year.get()}' and
+        prodCompany.name like '{producedBy.get()}%' and
+        prodCountry.name like '{productionCountry.get()}%' and
+        Genre.name like '{genre_selected.get()}%'
+        """
+
+    results_q = db.execute(sql_cmd)
+    results_list = list(results_q)
+    col_names = [col[0]  for col in results_q.description]
+    results_list.insert(0,col_names)
+    
+    text_box.insert(END,"\n\n")
+    text_box.insert(END,tabulate(results_list,tablefmt="pretty",headers='firstrow'))
+
+
+    export_button = Button(resultWindow,text="Export current table as CSV",width=20,bg="green")
+    export_button.pack(side=LEFT,fill="y")
+
+    
+    resultWindow.mainloop()
+
+
+
     
 
 
